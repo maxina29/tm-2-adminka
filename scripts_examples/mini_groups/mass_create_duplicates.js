5bc58993abb130afa014747ee229cf720157abb9
maxina29 <maxik1892@gmail.com>
2025-03-05 10:22:24 +0300

    mass create duplicates

--- scripts_examples/mini_groups/mass_create_duplicates.js
+++ scripts_examples/mini_groups/mass_create_duplicates.js
@@ -10,10 +10,9 @@ for (let id of ids) {
     tempWindow.document.querySelector('.my-btn.get-data').classList.add('bot-approve');
     tempWindow.document.querySelector('.my-btn.get-data').click();
     tempWindow.document.querySelector('#course_duplicate_group_templates_attributes_0_week_days_attributes_0_slot_week_day').value = 1;
-    tempWindow.document.querySelector('#course_duplicate_group_templates_attributes_0_week_days_attributes_0_slot_time').value = '10:00'
+    tempWindow.document.querySelector('#course_duplicate_group_templates_attributes_0_week_days_attributes_0_slot_time').value = '01:00'
     tempWindow.document.querySelector('#course_duplicate_group_templates_attributes_0_starts_at').value = '08.09.2025';
     tempWindow.document.querySelector('#course_duplicate_group_templates_attributes_0_schedule_hidden').checked = true;
-    tempWindow.document.querySelector('#course_duplicate_copy_preparation_materials').checked = true;
     tempWindow.document.querySelector('#course_duplicate_copy_groups').checked = false;
     while (!tempWindow.document.querySelector('#course_duplicate_group_templates_attributes_0_teacher_id').value) { await sleep(100); }
     tempWindow.document.querySelector('.btn.btn-default.btn-primary').click();
