b76d5ed1e378ce3db511e4ae9480eae72e42eb0b
maxina29 <maxik1892@gmail.com>
2025-03-05 11:37:40 +0300

    formating

--- scripts_examples/mini_groups/groups_set_location_mini.js
+++ scripts_examples/mini_groups/groups_set_location_mini.js
@@ -1,16 +1,19 @@
 // Проставляем локацию «Мини-группа» в первых параллелях курсов
-let courseIds=`10609
+let courseIds = `10609
 12345`.split('\n');
 let tempWindow = await createWindow();
-for (let courseId of courseIds){
-log(courseId)
-await tempWindow.openPage(`https://foxford.ru/admin/courses/${courseId}/groups`);
-tempWindow.querySelector('[id^="location_selector_"][name="group_template[default_location_id]"]').value=8;
-tempWindow.querySelector('[id^="format_selector_"][name="group_template[default_format_id]"]').value=1;
-let opt=tempWindow.createElement('option'); opt.value=60; opt.innerHTML='magic'; 
-let select=tempWindow.querySelector('[id^="studio_selector_"][name="group_template[default_studio_id]"]'); 
-select.appendChild(opt); select.value=60;
-select.closest('form').querySelector('[type="submit"]').click();
-await tempWindow.waitForElement('.alert-success');
+for (let courseId of courseIds) {
+    log(courseId)
+    await tempWindow.openPage(`https://foxford.ru/admin/courses/${courseId}/groups`);
+    tempWindow.querySelector('[id^="location_selector_"][name="group_template[default_location_id]"]').value = 8;
+    tempWindow.querySelector('[id^="format_selector_"][name="group_template[default_format_id]"]').value = 1;
+    let optionElement = tempWindow.createElement('option');
+    optionElement.value = 60;
+    optionElement.innerHTML = 'не нужна (мини-группы)';
+    let selectElement = tempWindow.querySelector('[id^="studio_selector_"][name="group_template[default_studio_id]"]');
+    selectElement.appendChild(optionElement);
+    selectElement.value = 60;
+    selectElement.closest('form').querySelector('[type="submit"]').click();
+    await tempWindow.waitForElement('.alert-success');
 }
 await tempWindow.close();
