72fe694e4759cfa45ecd5560309de5dca6235a7a
maxina29 <maxik1892@gmail.com>
2025-03-05 12:23:12 +0300

    mass reset schedule

---
+++ scripts_examples/mini_groups/mass_reset_schedule.js
@@ -0,0 +1,16 @@
+// Массовое перестроение расписания первых параллелей курса
+// Убедитесь, что в курсе стоит правильное расписание
+// почему-то иногда пропускает код, разобраться
+clear();
+let courseIds = `10609
+12345`.split('\n');
+let tempWindow = await createWindow();
+for (let courseId of courseIds) {
+    log(courseId);
+    await tempWindow.openPage(`https://foxford.ru/admin/courses/${courseId}/groups`);
+    tempWindow.querySelector('#from_lesson_number').value = '1';
+    tempWindow.querySelector('[id^="start_from_date_"]').value = '07.09.2025';
+    tempWindow.querySelectorAll('.btn-primary')[2].click();
+    await tempWindow.waitForElement('.alert-success');
+}
+await tempWindow.close();
