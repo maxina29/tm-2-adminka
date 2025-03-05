4d0df28c86bb5d534b806e28ce7d51e8e1132b7f
maxina29 <maxik1892@gmail.com>
2025-03-05 10:35:12 +0300

    restore product_pack_id

---
+++ scripts_examples/mini_groups/restore_product_pack_ids.js
@@ -0,0 +1,18 @@
+// вернуть подписку
+clear();
+let coursesInfo = {
+    10609: { 'course_product_pack_id': 12853 },
+    12345: { 'course_product_pack_id': 12862 },
+}
+let tempWindow = await createWindow();
+for (let courseId in coursesInfo) {
+    log(courseId);
+    await tempWindow.openPage(`https://foxford.ru/admin/mini_groups/${courseId}/edit`);
+    let courseInfo = coursesInfo[courseId];
+    for (let selector in courseInfo) {
+        tempWindow.querySelector(`#${selector}`).value = courseInfo[selector];
+    }
+    tempWindow.querySelector(`#course_name`).closest('form').querySelector('[type="submit"]').click();
+    await tempWindow.waitForElement('.alert-success');
+}
+tempWindow.close();
