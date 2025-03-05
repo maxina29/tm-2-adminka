460bf75320f9b4b6b5172cf37c7f338e18c5eb98
maxina29 <maxik1892@gmail.com>
2025-03-05 10:21:05 +0300

    format document

---
+++ scripts_examples/mini_groups/mass_create_duplicates.js
@@ -0,0 +1,22 @@
+// Массовое создание дубликатов мини-групп
+clear();
+let ids = `10609
+12345`.split('\n');
+let tempWindow = window.open('about:blank', 'adminka');;
+for (let id of ids) {
+    log(id);
+    tempWindow.location.href = 'https://foxford.ru/admin/courses/' + id + '/course_duplicates/new';
+    while (!tempWindow.document.querySelector('.my-btn.get-data')) { await sleep(100); }
+    tempWindow.document.querySelector('.my-btn.get-data').classList.add('bot-approve');
+    tempWindow.document.querySelector('.my-btn.get-data').click();
+    tempWindow.document.querySelector('#course_duplicate_group_templates_attributes_0_week_days_attributes_0_slot_week_day').value = 1;
+    tempWindow.document.querySelector('#course_duplicate_group_templates_attributes_0_week_days_attributes_0_slot_time').value = '10:00'
+    tempWindow.document.querySelector('#course_duplicate_group_templates_attributes_0_starts_at').value = '08.09.2025';
+    tempWindow.document.querySelector('#course_duplicate_group_templates_attributes_0_schedule_hidden').checked = true;
+    tempWindow.document.querySelector('#course_duplicate_copy_preparation_materials').checked = true;
+    tempWindow.document.querySelector('#course_duplicate_copy_groups').checked = false;
+    while (!tempWindow.document.querySelector('#course_duplicate_group_templates_attributes_0_teacher_id').value) { await sleep(100); }
+    tempWindow.document.querySelector('.btn.btn-default.btn-primary').click();
+    while (!tempWindow.document.querySelector('.courses_table')) { await sleep(100); }
+}
+tempWindow.close();
