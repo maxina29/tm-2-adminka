b9943f0237307eb392a78e906467b8a68e19e51f
maxina29 <maxik1892@gmail.com>
2025-03-31 19:24:25 +0300

    easy_change

---
+++ scripts_examples/mini_groups/mass_easy_changes_on_courses_page.js
@@ -0,0 +1,27 @@
+// делает то же, что и mass_change_full_name.js, но другим способом (быстрее)
+// меняет максимум одно поле в курсе за раз, но без открытия новых вкладок и с задержкой в секунду
+// запускать на странице https://foxford.ru/admin/courses
+// могут поддерживаться не все основные tag, перед массовым запуском проверить на парочке курсов
+// обязательно проверить по выгрузке, что все изменения подтянулись 
+let courseIds = `10609
+12345`.split('\n');
+let tag = 'visible_in_list'; //         or 'school_year_id'
+let newValue = 'true'; // or 'false'    or '14' for 2025-2026
+for (let courseId of courseIds) {
+    log(courseId);
+    let elem = document.querySelector('span[id^=best]');
+    let newElem = elem.cloneNode(true);
+    newElem.setAttribute('data-bip-attribute', tag);
+    newElem.setAttribute('data-bip-url', `/admin/courses/${courseId}`);
+    newElem.setAttribute('id', `best_in_place_course_${courseId}_${tag}`);
+    document.querySelector('tbody').appendChild(newElem);
+    newElem.click();
+    await sleep(100);
+    newElem.querySelector('input').value = newValue;
+    let submitButton = document.createElement('button');
+    submitButton.type = 'submit';
+    newElem.querySelector('form').appendChild(submitButton);
+    submitButton.click();
+    await sleep(900);
+}
+log('done');
