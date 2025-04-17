9115da6af96e7fe59c8e89a96dc3355219544d15
maxina29 <maxik1892@gmail.com>
2025-04-17 15:30:08 +0300

    dev-services-optimisation

--- scripts_examples/dev_services_mass_location.js
+++ scripts_examples/dev_services_mass_location.js
@@ -1,14 +1,14 @@
 // Массово проставляем локацию Шлак через дев-сервисы по ID параллели
 // Для проставления локации в настройки параллели используйте groups_set_location.js
 // Проверьте результат работы скрипта по отчету
-// В ссылке slag можно поменять на home или ssm или mini
 clear();
 let groupTemplateIds = `234567
 345678`.split('\n');
+let location = 'slag' // можно поменять на home, ssm или mini
 let win = await createWindow();
 for (let groupTemplateId of groupTemplateIds) {
     log(groupTemplateId);
-    await win.openPage(`https://foxford.ru/admin/dev_services?only_week_day_webinars_settings&select_group_template=${groupTemplateId}&slag&auto_validate`);
+    await win.openPage(`https://foxford.ru/admin/dev_services?only_week_day_webinars_settings&select_group_template=${groupTemplateId}&${location}&auto_validate`);
     await win.waitForSuccess();
 }
 win.close();
