09cc42cb1704871f56f9d747fedd199124440459
maxina29 <maxik1892@gmail.com>
2025-04-17 12:17:06 +0300

    restructurisation-create-duplicates-and-reset-schedule

--- scripts_examples/courses/mass_create_duplicates.js
+++ scripts_examples/courses/mass_create_duplicates.js
@@ -1,9 +1,8 @@
 // Массовое создание пачки дубликатов разных мини-групп
-// Если возникает ошибка 422, проверьте, не привязана ли к курсу подписка и отвяжите ее (clear_product_pack_ids.js)
-// Иногда также может не получаться из-за описания длинее 260 символов (пока встречалось редко, поэтому правим руками)
+// При ошибке 422 проверьте по гайду почему так может быть и исправьте это
 // После создания дублей можно 
 // - поменять массово разные поля (mass_change_fields_on_edit_page.js)
-// - массово проставить локацию МГ на странице groups (groups_set_location_mini.js)
+// - массово проставить локацию МГ на странице groups (groups_set_location.js)
 // - массово проставить локацию МГ в занятиях через дев-сервисы (dev_services_mass_location.js)
 // - массово перестроить параллели (mass_reset_schedule.js)
 clear();
@@ -20,6 +19,7 @@ for (let courseId of courseIds) {
     tempWindow.querySelector('#course_duplicate_group_templates_attributes_0_week_days_attributes_0_slot_time').value = '01:00'
     tempWindow.querySelector('#course_duplicate_group_templates_attributes_0_starts_at').value = '08.09.2025';
     tempWindow.querySelector('#course_duplicate_group_templates_attributes_0_schedule_hidden').checked = true; // расписание скрыто
+    tempWindow.querySelector('#course_duplicate_copy_preparation_materials').checked = true; // копировать подготовительные материалы
     tempWindow.querySelector('#course_duplicate_copy_groups').checked = false; // копировать прошедшие материалы 
     tempWindow.querySelector('#course_duplicate_video_available_on_schedule').checked = false; // сбросить стейт вебинаров 
     tempWindow.querySelector('#course_duplicate_asynchronous_access').checked = false; // асинхронный доступ
