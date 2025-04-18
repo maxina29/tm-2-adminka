b6fd0e4a36c8ff83541140180f5d51f8bc3aa42b
maxina29 <maxik1892@gmail.com>
2025-04-18 11:46:47 +0300

    remove_trainings

---
+++ scripts_examples/content/remove_trainings.js
@@ -0,0 +1,22 @@
+// Удаляет все тесты из курса
+// Как из занятий типа «Тест», так и из занятий типа «Перевернутое»
+// (из выбранных занятий в поле «Массовые правки вносятся с Занятие N по Занятие M (включительно)»)
+clear();
+let trainingWindow = await createWindow('adminka-tr');
+let lessonsList = currentWindow.querySelectorAll('[id^="edit_lesson_"]');
+for (let num = currentWindow.firstLessonNumber; num <= currentWindow.lastLessonNumber; num++) {
+    let lessonElement = lessonsList[num];
+    let greenLinkTrainingElement = lessonElement.querySelector('.green_link[href*="trainings"]')
+    if (greenLinkTrainingElement != null) {
+        log(num + 1);
+        greenLinkTrainingElement.target = 'adminka-tr';
+        greenLinkTrainingElement.click();
+        await trainingWindow.waitForElement('.loaded');
+        let deleteButton = trainingWindow.querySelector('[data-method="delete"][title="Отвязать"]');
+        deleteButton.click();
+        await trainingWindow.waitForSuccess();
+        await trainingWindow.openPage('about:blank');
+    }
+}
+await trainingWindow.close();
+await currentWindow.reload();
