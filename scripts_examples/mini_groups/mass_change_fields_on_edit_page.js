280491e162b16e817e01aa07e8f38305914fdc2c
maxina29 <maxik1892@gmail.com>
2025-04-23 15:39:54 +0300

    correct values for checkmarks

--- scripts_examples/mini_groups/mass_change_fields_on_edit_page.js
+++ scripts_examples/mini_groups/mass_change_fields_on_edit_page.js
@@ -16,11 +16,11 @@ for (let courseId of courseIds) {
     tempWindow.querySelector('#course_purchase_mode').value = 'enabled';
     tempWindow.querySelector('#course_predefined_full_price').value = '31490';
     tempWindow.querySelector('#course_standard_price').value = '31490';
-    tempWindow.querySelector('#course_presale').checked = 'checked';
+    tempWindow.querySelector('#course_presale').checked = true;
     tempWindow.querySelector('#course_mini_group_users_limit').value = '8';
-    tempWindow.querySelector('#course_published').checked = 'checked';
-    tempWindow.querySelector('#course_pin_top').checked = '';
-    tempWindow.querySelector('#course_visible_in_list').checked = '';
+    tempWindow.querySelector('#course_published').checked = true;
+    tempWindow.querySelector('#course_pin_top').checked = false;
+    tempWindow.querySelector('#course_visible_in_list').checked = false;
     tempWindow.querySelector('#course_product_pack_id').value = ''; // id привязанной подписки
     tempWindow.querySelector('#course_producer_id').value = '1867'; // Катя Афиногенова
     tempWindow.querySelector('#course_mini_group_landing_feature_attributes_free_lesson').checked = '';
