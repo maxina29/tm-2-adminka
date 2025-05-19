876c5e8bd551c39e41628aa8859f7d699147f5d4
maxina29 <maxik1892@gmail.com>
2025-05-19 14:58:37 +0300

    product pack mass different change

---
+++ scripts_examples/product_packs/mass_different_change.js
@@ -0,0 +1,20 @@
+// Меняем массово разные поля в курсах по-разному
+// Вместо product_pack_business_segment_tag_id и product_pack_product_segment_tag_id можно использовать любые ID текстовых полей со страницы /edit, в любом количестве
+// Сгенерируйте подобный объект через формулы в яндекс/гугл-таблицах
+clear();
+let productPacksInfo = {
+    9932: { 'product_pack_business_segment_tag_id': '683', 'product_pack_product_segment_tag_id': '1018' },
+    12527: { 'product_pack_business_segment_tag_id': '681', 'product_pack_product_segment_tag_id': '713' },
+}
+let tempWindow = await createWindow();
+for (let productPackId in productPacksInfo) {
+    log(productPackId);
+    await tempWindow.openPage(`https://foxford.ru/admin/product_packs/${productPackId}/edit`);
+    let productPackInfo = productPacksInfo[productPackId];
+    for (let selector in productPackInfo) {
+        tempWindow.querySelector(`#${selector}`).value = productPackInfo[selector];
+    }
+    tempWindow.querySelector(`#product_pack_name`).closest('form').querySelector('[type="submit"]').click();
+    await tempWindow.waitForSuccess();
+}
+await tempWindow.close();
