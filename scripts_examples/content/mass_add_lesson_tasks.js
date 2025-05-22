91ef30d78865536d783866952680bc634928dacf
maxina29 <maxik1892@gmail.com>
2025-05-22 11:37:39 +0300

    багфикс: в списке задач могли дублироваться задачи

--- scripts_examples/content/mass_add_lesson_tasks.js
+++ scripts_examples/content/mass_add_lesson_tasks.js
@@ -4,7 +4,7 @@
 // https://metabase.foxford.ru/question/28411
 let data = {
     327200: '204837 37659   205591 109398',
-    327201: '213222 205531 290653 290662 290663',
+    327201: '213222 205531  290653 290653 290662 290663',
     327202: '72241 374566 374567',
 }
 clear();
@@ -14,19 +14,23 @@ for (let lessonId in data) {
     log(`Начинаю урок ${lessonId}`)
     await lessonTasksWindow.openPage(`https://foxford.ru/admin/lessons/${lessonId}/lesson_tasks?q%5Bdisciplines_id_in%5D=`);
     let tbody = lessonTasksWindow.querySelector('.task_table tbody:not(.sortable)');
-    let tasks = data[lessonId].split(/\s+/);
+    let tasks = new Set(data[lessonId].split(/\s+/));
     for (let num = 25; num < tasks.length; num++) {
         let tr = tbody.firstChild.cloneNode(true);
         tbody.appendChild(tr);
     }
     let trs = tbody.childNodes;
-    for (let num = 0; num < tasks.length; num++) {
+    let num = 0
+    for (let taskId of tasks) {
         let btn = trs[num].querySelector('a.btn-default[rel]');
-        btn.href = btn.href.substr(0, btn.href.search('=') + 1) + tasks[num];
+        btn.href = btn.href.substr(0, btn.href.search('=') + 1) + taskId;
         btn.target = 'adminka_tmp'
         btn.click();
         await tempWindow.waitForSuccess();
         await tempWindow.openPage('about:blank');
-        log(tasks[num]);
+        log(taskId);
+        num++;
     }
 }
+await tempWindow.close();
+await lessonTasksWindow.close();
