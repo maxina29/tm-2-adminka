fe2cf6d650cca425a4de5f8e1e77ee0db3f3f6a9
maxina29 <maxik1892@gmail.com>
2025-10-27 15:38:02 +0300

    fix: некоторые исправления checkGroupsOnSchedule

--- tm-2-0.user.js
+++ tm-2-0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TestAdminka-beta14
 // @namespace    https://uploads-foxford-ru.ngcdn.ru/
-// @version      0.2.0.95-beta14-0.5
+// @version      0.2.0.95-beta14-0.6
 // @description  Улучшенная версия админских инструментов
 // @author       maxina29, wanna_get_out && deepseek
 // @match        https://beta14.100ege.ru/admin*
@@ -2113,6 +2113,8 @@ const pagePatterns = {
     // на странице с расписанием
     if (currentWindow.checkPath(pagePatterns.groups) && true) {
         group_template_id.classList.add('protected');
+
+        // Автоматическая простановка преподавателя для МГ и тренажерных курсов
         let courseTypeId = course_data.dataset.courseTypeId;
         if (['5', '6'].includes(courseTypeId)) {
             let newTemplateTeacher = currentWindow.querySelector('.new_group_template #group_template_teacher_id');
@@ -2122,8 +2124,11 @@ const pagePatterns = {
             newTemplateTeacher.dispatchEvent(new Event('change'));
             newTemplateTeacher.setAttribute('readonly', 'true');
         }
-        document.querySelector('[data-target="#new_group_template"]').classList.replace('btn-default', 'btn-success');
-        let lessonRows = currentWindow.querySelectorAll('.panel[id^="group_"]');
+
+        // Подготовка страницы
+        currentWindow.querySelector('[data-target="#new_group_template"]')
+            .classList.replace('btn-default', 'btn-success');
+        let lessonRows = Array.from(currentWindow.querySelectorAll('.panel[id^="group_"]'));
         for (let lessonRow of lessonRows) {
             lessonRow.classList.add('lesson_row');
             let headingRow = lessonRow.querySelector('.panel-heading');
@@ -2169,9 +2174,10 @@ const pagePatterns = {
             }
         }
         let adminButtons = createElement('div', 'adminButtons');
-        let groupsPage = document.querySelector('.groups_page');
+        let groupsPage = currentWindow.querySelector('.groups_page');
         groupsPage.prepend(adminButtons);
 
+        // Кнопка «↑ Перестроить ↑»
         async function rebuildUpButtonOnClick() {
             rebuildUpButton.style = 'display:none';
             let hasFutureLesson = false;
@@ -2211,6 +2217,147 @@ const pagePatterns = {
             rebuildUpButton.classList.add('bot-approve');
             rebuildUpButton.click();
         }
+
+        // Генерация уведомлений от @wanna_get_out
+        function alertManager() {
+            const managerId = 'alert-manager-container';
+            let container = currentWindow.querySelector(`#${managerId}`);
+
+            if (!container) {
+                container = currentWindow.createElement('div');
+                container.id = managerId;
+                container.style.display = 'flex';
+                container.style.flexDirection = 'column';
+                container.style.gap = '10px';
+                container.style.marginTop = '10px';
+
+                const referenceNode = currentWindow.querySelector('#course_data');
+                if (referenceNode) {
+                    referenceNode.parentNode.insertBefore(container, referenceNode);
+                } else {
+                    currentWindow.body.prepend(container);
+                }
+            }
+
+            return {
+                addAlert: (message, bgColor = '#ffb6c4', alertClass = 'custom-alert') => {
+                    const existingAlert = container.querySelector(`.${alertClass}`);
+                    if (existingAlert) existingAlert.remove();
+
+                    const alert = currentWindow.createElement('div');
+                    alert.className = alertClass;
+                    alert.style.padding = '10px';
+                    alert.style.borderRadius = '4px';
+                    alert.style.backgroundColor = bgColor;
+                    alert.style.textAlign = 'center';
+                    alert.textContent = message;
+
+                    container.appendChild(alert);
+                },
+
+                removeAlert: (alertClass = 'custom-alert') => {
+                    const alert = container.querySelector(`.${alertClass}`);
+                    if (alert) alert.remove();
+                }
+            };
+        }
+
+        // Проверка, что все занятия по расписанию от @wanna_get_out
+        let checkGroupsOnSchedule = async function () {
+
+            // Возвращает список дней недели
+            function getWeekdays() {
+                const blocks = [];
+                const times = [];
+                for (let i = 0; i < 7; i++) {
+                    let element = currentWindow.querySelector
+                        (`#group_template_week_days_attributes_${i}_slot_week_day option[selected]`);
+                    let timeElement = currentWindow.querySelector
+                        (`#edit_group_template #group_template_week_days_attributes_${i}_slot_time`);
+                    if (element) blocks.push(Number(element.value));
+                    if (timeElement) times.push(timeElement.value.split(':'));
+                }
+                return [blocks, times]
+            }
+
+            // Список с датами уроков по расписанию
+            function trueLesssonDates() {
+                const days = [];
+                const startDateElement = currentWindow.querySelector('#edit_group_template [id*="starts_at_date"]');
+                const startDate = new Date(startDateElement.value.split('.').reverse());
+                const [weekdays, weekdayTimes] = getWeekdays();
+
+                // Создаем копиию начальной даты, чтобы не изменять исходную
+                let date = new Date(startDate);
+                date.setHours(weekdayTimes[0][0], weekdayTimes[0][1], 0, 0);
+
+                let startsAtElements = lessonRows.map(x => x.querySelector('[id*="starts_at_"]')).filter(Boolean);
+                let maxDate = new Date(
+                    Math.max(...startsAtElements.map(el => new Date(el.value.split(' ')[0].split('.').reverse())))
+                );
+                maxDate.setHours(23, 59, 59, 999);
+                while (date <= maxDate) {
+                    for (let i = 0; i < weekdays.length; i++) {
+                        // Если день недели есть в списке дней недели и он i-ый
+                        if (weekdays[i] == Number(date.getDay())) {
+                            let tempDate = new Date(date);
+                            tempDate.setHours(weekdayTimes[i][0], weekdayTimes[i][1], 0, 0)
+                            days.push(Number(tempDate));
+                        }
+                    }
+                    // Увеличиваем дату на один день
+                    date.setDate(date.getDate() + 1);
+
+                }
+                return days;
+            }
+            let trueLessons = trueLesssonDates();
+
+            // Получаем все уроки с лендинга
+            let allLessonsStartDate = Array.from(currentWindow.querySelectorAll('[id*="starts_at_"]')).slice(2);
+            const bgColorEven = '#ff869d';
+            const bgColorOdd = '#ffb6c4';
+            let lessonsCount = 0;
+
+            // Проходим циклом по всем датам уроков
+            for (let i = 0; i < allLessonsStartDate.length; i++) {
+                // Преобразовываем дату с лендинга в человеческую
+                let date = new Date(allLessonsStartDate[i].value.split(' ')[0].split('.').reverse());
+                let time = allLessonsStartDate[i].value.split(' ')[1].split(':');
+                date.setHours(time[0], time[1], 0, 0);
+                date = Number(date);
+                // Ищем совпадение даты лендинга с датой по расписанию
+                if (!trueLessons.includes(date)) {
+                    // Если не совпадает с расписанием (перенос, переназначение), красим родительский элемент 
+                    // (родительского элемента родительского элемента...) в цвет
+                    let parent = allLessonsStartDate[i].parentElement.parentElement.parentElement.parentElement;
+                    if (lessonsCount % 2 == 0) {
+                        parent.style.backgroundColor = bgColorEven;
+                    } else {
+                        parent.style.backgroundColor = bgColorOdd;
+                    }
+                    lessonsCount += 1;
+                }
+                else {
+                    let parent = allLessonsStartDate[i].parentElement.parentElement.parentElement.parentElement;
+                    parent.style.backgroundColor = '';
+                }
+            }
+            const alerts = alertManager()
+            if (lessonsCount) {
+                alerts.addAlert(
+                    `В данной параллели занятий не по расписанию: ${lessonsCount}`,
+                    bgColorOdd,
+                    'no-rasp-alert'
+                );
+            } else {
+                alerts.removeAlert('no-rasp-alert');
+            }
+            currentWindow.body.firstChild.className += ' rasp_checked';
+        }
+        let saveButtons = currentWindow.querySelectorAll('.btn-default[value="Сохранить"]');
+        for (let saveButton of saveButtons) saveButton.addEventListener('click', checkGroupsOnSchedule);
+        checkGroupsOnSchedule();
 
         log('Страница модифицирована');
     }
@@ -2633,156 +2780,6 @@ const pagePatterns = {
         }
         full_numeration_creation();
 
-        // Генерация уведомлений от @wanna_get_out
-        function alertManager() {
-            const managerId = 'alert-manager-container';
-            let container = currentWindow.querySelector(`#${managerId}`);
-
-            if (!container) {
-                container = currentWindow.createElement('div');
-                container.id = managerId;
-                container.style.display = 'flex';
-                container.style.flexDirection = 'column';
-                container.style.gap = '10px';
-                container.style.marginTop = '10px';
-
-                const referenceNode = currentWindow.querySelector('#course_data');
-                if (referenceNode) {
-                    referenceNode.parentNode.insertBefore(container, referenceNode);
-                } else {
-                    currentWindow.body.prepend(container);
-                }
-            }
-
-            return {
-                addAlert: (message, bgColor = '#ffb6c4', alertClass = 'custom-alert') => {
-                    const existingAlert = container.querySelector(`.${alertClass}`);
-                    if (existingAlert) existingAlert.remove();
-
-                    const alert = currentWindow.createElement('div');
-                    alert.className = alertClass;
-                    alert.style.padding = '10px';
-                    alert.style.borderRadius = '4px';
-                    alert.style.backgroundColor = bgColor;
-                    alert.style.textAlign = 'center';
-                    alert.textContent = message;
-
-                    container.appendChild(alert);
-                },
-
-                removeAlert: (alertClass = 'custom-alert') => {
-                    const alert = container.querySelector(`.${alertClass}`);
-                    if (alert) alert.remove();
-                }
-            };
-        }
-
-        // занятия не по расписанию от @wanna_get_out
-        let no_rasp_groups = async function () {
-            // Возвращает список дней недели
-            function getWeekdays() {
-                const blocks = [];
-                const times = [];
-
-                for (let i = 0; i < 7; i++) {
-                    let element = document.querySelector
-                        ('#' + `group_template_week_days_attributes_${i}_slot_week_day` + ' option[selected]')
-                    let element_time = document.querySelector
-                        ('#edit_group_template #' + `group_template_week_days_attributes_${i}_slot_time`)
-                    if (element) {
-                        blocks.push(Number(element.value));
-                    }
-                    if (element_time) {
-                        times.push(element_time.value.split(':'))
-                    }
-                }
-                return [blocks, times]
-            }
-
-            // Список с датами уроков по расписанию
-            function trueLesssonDates() {
-                const days = [];
-                const startDate = new Date(
-                    window.document.querySelectorAll('[id*="starts_at_date"]')[1].value.split('.').reverse()
-                );
-                let landingLessonCount = window.document.querySelectorAll('.lesson_number').length;
-                const [weekdays, weekday_times] = getWeekdays();
-
-                // Создаем копиию начальной даты, чтобы не изменять исходную
-                let date = new Date(startDate);
-                // Переводим дату к нужному времени
-                date.setHours(weekday_times[0][0], weekday_times[0][1], 0, 0);
-                // получаем максимальную дату
-                let allLessonsStartDate = Array.from(window.document.querySelectorAll('[id*="starts_at_"]')).slice(2);
-                let maxDate = new Date(Math.max(...allLessonsStartDate.map(x => new Date(
-                    x.value.split(' ')[0].split('.').reverse()
-                ))));
-                maxDate.setHours(23, 59, 59, 999);
-                //while (landingLessonCount >= 0) {
-                while (date <= maxDate) {
-                    for (let i = 0; i < weekdays.length; i++) {
-                        // Если день недели есть в списке дней недели и он i-ый
-                        if (weekdays[i] == Number(date.getDay())) {
-                            let temp_date = new Date(date);
-                            temp_date.setHours(weekday_times[i][0], weekday_times[i][1], 0, 0)
-                            days.push(Number(temp_date));
-                            landingLessonCount--; // Уменьшаем счётчик уроков
-                        }
-                    }
-                    // Увеличиваем дату на один день
-                    date.setDate(date.getDate() + 1);
-
-                }
-                return days;
-            }
-
-            // const weekdays = getWeekdays();
-            let trueLessons = trueLesssonDates();
-
-            // Получаем все уроки с лендинга
-            let allLessonsStartDate = Array.from(window.document.querySelectorAll('[id*="starts_at_"]')).slice(2);
-            const bgColorEven = '#ff869d';
-            const bgColorOdd = '#ffb6c4';
-            let lessonsCount = 0;
-
-            // Проходим циклом по всем датам уроков
-            for (let i = 0; i < allLessonsStartDate.length; i++) {
-                // Преобразовываем дату с лендинга в человеческую
-                let date = new Date(allLessonsStartDate[i].value.split(' ')[0].split('.').reverse());
-                let time = allLessonsStartDate[i].value.split(' ')[1].split(':');
-                date.setHours(time[0], time[1], 0, 0);
-                date = Number(date);
-                // Ищем совпадение даты лендинга с датой по расписанию
-                if (!trueLessons.includes(date)) {
-                    // Если не совпадает с расписанием (перенос, переназначение), красим родительский элемент 
-                    // (родительского элемента родительского элемента...) в цвет
-                    let parent = allLessonsStartDate[i].parentElement.parentElement.parentElement.parentElement;
-                    if (lessonsCount % 2 == 0) {
-                        parent.style.backgroundColor = bgColorEven;
-                    } else {
-                        parent.style.backgroundColor = bgColorOdd;
-                    }
-                    lessonsCount += 1;
-                }
-                else {
-                    let parent = allLessonsStartDate[i].parentElement.parentElement.parentElement.parentElement;
-                    parent.style.backgroundColor = '';
-                }
-            }
-            const alerts = alertManager()
-            if (lessonsCount) {
-                alerts.addAlert(
-                    `В данной параллели занятий не по расписанию: ${lessonsCount}`,
-                    bgColorOdd,
-                    'no-rasp-alert'
-                );
-            } else {
-                alerts.removeAlert('no-rasp-alert');
-            }
-            document.body.firstChild.className += ' rasp_checked';
-        }
-        no_rasp_groups();
-
         function templateStartDateChecker() {
 
             function checkDates() {
@@ -2910,11 +2907,8 @@ const pagePatterns = {
                 if (!win.closed) win.close();
             }
         }
-
         changeWebinarLocations();
 
-        let all_save_btns = document.querySelectorAll('.btn-default[value="Сохранить"]');
-        for (let save_btn of all_save_btns) { save_btn.addEventListener('click', no_rasp_groups); }
         //
         let set_all_duration_at_ = async function (x = 40) {
             let a = document.getElementsByClassName('groups_table')[0].getElementsByTagName('tr');
@@ -5298,7 +5292,7 @@ for (let [trainingId, newName] of pairs) {
         mainPage.appendChild(fvsButton);
         mainPage.appendChild(foxButton);
         mainPage.querySelector('p').innerHTML +=
-            `<br>Установлены скрипты Tampermonkey 2.0 (v.0.2.0.95-beta14-0.5 от 27 октября 2025)
+            `<br>Установлены скрипты Tampermonkey 2.0 (v.0.2.0.95-beta14-0.6 от 27 октября 2025)
             <br>Примеры скриптов можно посмотреть 
             <a href="https://github.com/maxina29/tm-2-adminka/tree/main/scripts_examples" target="_blank">здесь</a>
             <br><a href="/tampermoney_script_adminka.user.js" target="_blank">Обновить скрипт</a>`;
