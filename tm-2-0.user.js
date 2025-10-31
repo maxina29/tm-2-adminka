7c7b8a96ed3389bb1727290187beb1a088c96cba
maxina29 <maxik1892@gmail.com>
2025-10-31 10:13:57 +0300

    fix: Добавил количество занятий в уведомление

--- tm-2-0.user.js
+++ tm-2-0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TestAdminka
 // @namespace    https://uploads-foxford-ru.ngcdn.ru/
-// @version      0.2.0.109
+// @version      0.2.0.110
 // @description  Улучшенная версия админских инструментов
 // @author       maxina29, wanna_get_out && deepseek
 // @match        https://foxford.ru/admin*
@@ -2628,7 +2628,8 @@ const pagePatterns = {
                     group => `${group.lesson_number} (id ${group.group_id}) - ${group.starts_at}`
                 ).join('; ');
                 alerts.addAlert(
-                    `В данной параллели есть занятия не по расписанию:<br>${formattedDates}`,
+                    `В данной параллели есть занятия не по расписанию (${сompensatingLessons.length}):<br>` +
+                    `${formattedDates}`,
                     'no-rasp-alert', bgColorOdd,
                 );
                 currentWindow.specialData.states.hasСompensatingLesson = true;
@@ -2638,7 +2639,7 @@ const pagePatterns = {
             }
             if (skippedDates.length) {
                 alerts.addAlert(
-                    `В данной параллели есть пропущенные занятия:<br>${skippedDates.join('; ')}`,
+                    `В данной параллели есть пропущенные занятия (${skippedDates.length}):<br>${skippedDates.join('; ')}`,
                     'no-rasp-alert-skip', bgColorOdd,
                 );
             }
@@ -5686,7 +5687,7 @@ for (let [trainingId, newName] of pairs) {
         mainPage.appendChild(fvsButton);
         mainPage.appendChild(foxButton);
         mainPage.querySelector('p').innerHTML +=
-            `<br>Установлены скрипты Tampermonkey 2.0 (v.0.2.0.109 от 30 октября 2025)
+            `<br>Установлены скрипты Tampermonkey 2.0 (v.0.2.0.110 от 30 октября 2025)
             <br>Примеры скриптов можно посмотреть 
             <a href="https://github.com/maxina29/tm-2-adminka/tree/main/scripts_examples" target="_blank">здесь</a>
             <br><a href="/tampermoney_script_adminka.user.js" target="_blank">Обновить скрипт</a>`;
